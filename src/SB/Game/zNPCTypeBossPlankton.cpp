e399bbd3c6b63e5ed62d2f5bef2f2fdde7c8708c
Olaia <104322336+JustOlaia@users.noreply.github.com>
2026-04-21 18:43:23 -0600

    Update zNPCTypeBossPlankton.cpp

--- src/SB/Game/zNPCTypeBossPlankton.cpp
+++ src/SB/Game/zNPCTypeBossPlankton.cpp
@@ -1601,7 +1601,7 @@ void zNPCBPlankton::update_dialog(F32 dt)
     {
         if (t->fuse != NULL && !t->fuse_detected)
         {
-            if (zEntDestructObj_isDestroyed(t->fuse))) // has fuse been destroyed?
+            if (zEntDestructObj_isDestroyed(t->fuse)) // has fuse been destroyed?
             {
                 say(2, 0, false); // say_fuse_near
                 t->fuse_detected = 1;
